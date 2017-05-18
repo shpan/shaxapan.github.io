a4cd6eb1be04f81ec2e778b156ae205795e684d4
shaxapan <shaxapan@mail.ru>
2017-05-18 20:28:56 +0500

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 200) {
-       $('nav').addClass('bar-fixed');
-    }
-    else {
-       $('nav').removeClass('bar-fixed');
-    }
-});
