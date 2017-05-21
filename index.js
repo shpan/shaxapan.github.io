69920149f48813bcdd80bd298eabeed77ae022b3
shaxapan <shaxapan@mail.ru>
2017-05-22 00:11:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// protected mode voor wordpress
+$.noConflict();
+jQuery(document).ready(function($) {
+    // initial
+    $('#content').load('');
+
+	// menu clicked
+	$('ul#nav li a').click(function() {
+	    var page = $(this).attr('href');
+        $('#content').load('http://www.applestore.uz/' + page + '.html');
+	return false;
+	alert(page);
+	})
+})
