21b83fbff9d885101fc620c9cbdc3b6a48ba9180
shaxapan <shaxapan@mail.ru>
2017-05-22 00:17:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // protected mode voor wordpress
 $.noConflict();
 jQuery(document).ready(function($) {
-    // initial
-    $('#content').load('http://www.applestore.uz');
-
-	// menu clicked
 	$('ul#nav li a').click(function() {
 	    var page = $(this).attr('href');
         $('#content').load('http://www.applestore.uz/' + page + '.html');
